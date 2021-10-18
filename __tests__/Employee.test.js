b21870db9ebabe7d2ac1afe24ab725eab300c568
Rick Martin <rmartin1985@att.net>
2021-10-18 11:38:19 -0500

    test for employee name passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,17 @@
 const Employee = require('../lib/Employee');
 
 // test for creating the Employee class object
-const { TestWatcher } = require("@jest/core");
 
 test('creates an employee object', () => {
-    const employee = {
-        name: 'Rick',
-        id: 1,
-        email: 'rmartin1985@att.net'
-    };
+    const employee = new Employee('Rick', 1, 'rmartin1985@att.net');
 
     expect(employee.name).toBe('Rick');
     expect(employee.id).toBe(1);
     expect(employee.email).toBe('rmartin1985@att.net');
+})
+
+test('access employee name', () => {
+    const employee = new Employee('Rick', 1, 'rmartin1985@att.net');
+
+    expect(employee.getName()).toBe('Rick')
 })
